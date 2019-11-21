86e6e54994b7a5e8c71fac4823b95db79823752d
VHeusinkveld <vincent.heusinkveld@gmail.com>
2019-11-21 21:54:13 +0100

    Problem 14 added multiprocessing.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Solutions coded by me. Mostly in python but there is some matlab and C++. The repo is made public for my personal convinience and possibly for some people to learn from my implementations. 
 
 www.projecteuler.net
+
+Log:
+* 000 - 010: combination of python and matlab. Old solutions.
+* 011: python, cython
+* 012: cython, C++ 
+* 013: python 2x
+* 014: python, python multithreaded
---
+++ problem_14_multiprocessing.py
@@ -0,0 +1,53 @@
+import sys
+
+sys.setrecursionlimit(10000)
+
+import numpy as np
+import multiprocessing as mp
+
+from functools import reduce
+
+def get_next_step(original, data, depth, output):
+    depth += 1
+    mask_even = (data & 1) == 0
+    mask_odd = ~mask_even
+    
+    data[mask_even] = data[mask_even]/2
+    data[mask_odd] = 3*data[mask_odd] + 1
+    
+    mask_one = (data != 1)
+    mask_sum = np.sum(mask_one)
+    
+    if mask_sum != 1:
+        get_next_step(original[mask_one], data[mask_one], depth, output)
+    else:
+        output.put((depth, original[mask_one][0]))
+
+
+from time import time
+
+if __name__ ==  '__main__':
+    start = time()
+
+    tries = np.arange(1, int(1e6), dtype=np.int64)
+    np.random.shuffle(tries)
+
+    N = 2
+
+    splits = (np.arange(1, N) * len(tries)/N).astype(int)
+    tries = np.split(tries, splits)
+
+    output = mp.Queue()
+    processes = []
+    for data_i in tries:
+        p = mp.Process(target=get_next_step, args=(data_i, 1*data_i, 0, output))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    results = [output.get() for p in processes]
+    results.sort()
+    print(f'Loop depth, number: {results[-1]}')
+    print(f'time = {time()-start}')
